061e3180a855832ff9eaa6e274bbccf24477a32b
Masha <marija.bolschakova@yandex.ru>
2021-03-28 18:28:41 +0300

    do notification and fix some errors in column-chart

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,4 +1,6 @@
 export default class ColumnChart {
+  chartHeight = 50;
+
   constructor({
     data = [],
     label = '',
@@ -11,31 +13,14 @@ export default class ColumnChart {
     this.value = value;
     this.render();
   }
-  
-  chartHeight = 50;
-
-  update() {
-    const newColumnChart = document.querySelector('column-chart__chart');
-    newColumnChart.innerHTML = this.getColumns;
-  };
-
-  getColumns(data) {
-    const maxValue = Math.max(...data);
-    const scale = 50 / maxValue;
-      
-    return data.map(item => {
-      return {
-        percent: (item / maxValue * 100).toFixed(0) + '%',
-        value: String(Math.floor(item * scale))
-      };
-    });
-  };
 
   render() {
     const element = document.createElement('div'); // (*)
     
     element.innerHTML = `
-      <div class="column-chart column-chart_loading" style="--chart-height: ${this.chartHeight}">
+    <div class="column-chart ${this.data.length || 'column-chart_loading'}"
+    style="--chart-height: ${this.chartHeight}"
+>
       <div class="column-chart__title">
         Total ${this.label}
         ${this.link ? `<a class="column-chart__link" href="${this.link}">View all</a>` : ''}
@@ -54,12 +39,26 @@ export default class ColumnChart {
     // NOTE: в этой строке мы избавляемся от обертки-пустышки в виде `div`
     // который мы создали на строке (*)
     this.element = element.firstElementChild;
-
-    if (this.data.length === 0) {
-      element.innerHTML = `<img src="./charts-skeleton.svg">`
-    }
   }
-    
+
+
+  getColumns(data) {
+    const maxValue = Math.max(...data);
+    const scale = this.chartHeight / maxValue;
+      
+    return data.map(item => {
+        const percent = (item / maxValue * 100).toFixed(0) + '%';
+
+        return `<div style="--value: ${Math.floor(item * scale)}" data-tooltip="${percent}"></div>`;
+    }).join('');
+  }
+
+  update(updateData) {
+    updateData = this.data;
+    const newColumnChart = document.querySelector('column-chart__chart');
+    newColumnChart.innerHTML = this.getColumns;
+  };
+  
     remove () {
       this.element.remove();
     }
